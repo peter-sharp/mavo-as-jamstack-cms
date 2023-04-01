cbdacc1f1e25fd84d1d3ac3fd6434f79e1affa58
Peter Sharp <vsharp@protonmail.com>
2023-04-01 17:56:57 -0600

    Removes dynamic includes to be compatible with Github

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,11 @@ module.exports =  function (eleventyConfig) {
     eleventyConfig.addGlobalData('layout', 'default');
     // placeholder for now
     eleventyConfig.addFilter("relative_url", x => x);
+
+    eleventyConfig.setLiquidOptions({
+        dynamicPartials: false,
+        strictFilters: false, // renamed from `strict_filters` in Eleventy 1.0
+    });
     return {
         dir: {
             // ⚠️ These values are both relative to your input directory.
